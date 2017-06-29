347980543142606c23df0585d846af4045b13a4c
Andrea Parodi <andrea@parro.it>
2017-06-29 22:51:27 +0200

    key & code & modifiers check

--- main.js
+++ main.js
@@ -3,7 +3,14 @@ export default function areEqual(ev1, ev2) {
 		return true;
 	}
 
-	if (Boolean(ev1.altKey) !== Boolean(ev2.altKey)) {
+	for (const prop of ['altKey', 'ctrlKey', 'shiftKey', 'metaKey']) {
+		const [value1, value2] = [ev1[prop], ev2[prop]];
+		if (Boolean(value1) !== Boolean(value2)) {
+			return false;
+		}
+	}
+
+	if (ev1.key !== ev2.key || ev1.code !== ev2.code) {
 		return false;
 	}
 
--- test.js
+++ test.js
@@ -17,3 +17,43 @@ test('return false for different `altKey` props', t => {
 test('return true for false and absent `altKey` props', t => {
 	t.true(areEqual({}, {altKey: false}));
 });
+
+test('return false for different `ctrlKey` props', t => {
+	t.false(areEqual({ctrlKey: true}, {ctrlKey: false}));
+});
+
+test('return true for false and absent `ctrlKey` props', t => {
+	t.true(areEqual({}, {ctrlKey: false}));
+});
+test('return false for different `shiftKey` props', t => {
+	t.false(areEqual({shiftKey: true}, {shiftKey: false}));
+});
+
+test('return true for false and absent `shiftKey` props', t => {
+	t.true(areEqual({}, {shiftKey: false}));
+});
+
+test('return false for different `metaKey` props', t => {
+	t.false(areEqual({metaKey: true}, {metaKey: false}));
+});
+
+test('return false for different `key` props', t => {
+	t.false(areEqual({key: 'Delete'}, {key: 'CS'}));
+});
+
+test('return false for false and absent `key` props', t => {
+	t.false(areEqual({}, {key: 'Delete'}));
+});
+
+test('return false for false and absent `code` props', t => {
+	t.false(areEqual({}, {code: 'D'}));
+});
+
+test('return false for different `code` props', t => {
+	t.false(areEqual({code: 'D'}, {code: 'C'}));
+});
+
+test('return true for false and absent `metaKey` props', t => {
+	t.true(areEqual({}, {metaKey: false}));
+});
+
